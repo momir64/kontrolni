111301a40fa406b72fcc8c3dae2a89c66bc01472
Momir Stanišić <40437358+momir64@users.noreply.github.com>
2021-12-09 23:40:06 +0100

    Add files via upload

---
+++ ComboBoxItem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pecaros {
+    class ComboBoxItem {
+        public object Value;
+        public string Text;
+
+        public ComboBoxItem(object value, string text) {
+            Value = value;
+            Text = text;
+        }
+
+        public override string ToString() {
+            return Text;
+        }
+    }
+}
